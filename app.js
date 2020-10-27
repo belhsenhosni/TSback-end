f82d4f29f27332e5caebb1e713b9024b42332af1
Belhsen <belhsen.hosni@esprit.tn>
2020-10-27 10:14:05 +0100

    AuthentificationHeroku

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use("/api/users", userRouter);
 
-app.listen(process.env.APP_PORT, () => {
+/*app.listen(process.env.APP_PORT, () => {
     console.log("Server is running on port : ", process.env.APP_PORT);
-});
+});*/
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
